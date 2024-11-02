7c03c0654d4edee909f6dfa0a1e46f3b918e4ac6
Didier.Grand <didier.grand@scott-sports.com>
2024-11-02 13:28:01 +0100

    css changes buttons ans text sizes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,17 +11,17 @@ const Hero = () => {
         <section className="max-w-[1280px] mx-auto px-4 lg:px-8 grid grid-col-4 md:grid-col-6 lg:grid-cols-12 gap-12 md:gap-8 py-[120px]">
           {/* Left Content */}
           <div className="col-span-4 md:col-span-6 lg:col-span-5 flex flex-col justify-center gap-4 md:gap-6">
-            <h1 className="text-3xl sm:text-6xl lg:text-6xl font-[600] tracking-tight text-gray-900 ">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-[600] tracking-tight text-gray-900 ">
               Well crafted abstract images
             </h1>
             <p className="mt-4 lg:mt-6 text-base lg:text-xl leading-8 text-gray-600">
               High quality abstract images for your projects, wallpaper and presentations.
             </p>
-            <div className="mt-6 lg:mt-14 flex flex-row gap-8 items-center justify-start">
-              <button className="w-full sm:w-auto text-lg rounded-md bg-white px-6 md:px-10 py-4 text-gray-900 hover:bg-gray-100 transition-colors border border-inner border-neutral-200 shadow-sm">
+            <div className="mt-6 lg:mt-14 flex flex-row gap-4 md:gap-8 items-center justify-start">
+              <button className="w-full sm:w-auto text-lg rounded-md bg-white px-2 md:px-10 py-4 text-gray-900 hover:bg-gray-100 transition-colors border border-inner border-neutral-200 shadow-sm">
                 Learn more
               </button>
-              <button className="w-full sm:w-auto text-lg rounded-md bg-indigo-700 px-6 md:px-10 py-4 text-white hover:bg-indigo-900 transition-colors border border-indigo-700 shadow-sm">
+              <button className="w-full sm:w-auto text-lg rounded-md bg-indigo-700 px-2 md:px-10 py-4 text-white hover:bg-indigo-900 transition-colors border border-indigo-700 shadow-sm">
                 See pricing
               </button>
             </div>
